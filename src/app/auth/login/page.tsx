fa4ed5ad97cd31a765c0859baae3d73dd298d6d1
Aliaskar Urakov <urakov.aliaskar@gmail.com>
2025-10-21 00:08:05 -0700

    convert to server

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,11 +1,24 @@
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Separator } from "@/components/ui/separator";
 import { SignUpTab } from "./_components/sign-up-tab";
 import { SignInTab } from "./_components/sign-in-tab";
 import { SocialAuthButtons } from "./_components/social_auth_buttons";
+import { auth } from "@/lib/auth";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session !== null) redirect("/");
+
   return (
     <Tabs defaultValue="signin" className="w-full max-w-96 mx-auto my-6 px-4">
       <TabsList>
